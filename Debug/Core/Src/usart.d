ef455e40c5317a8a3fb0b78d25a2dece2722385b
louisont12 <tetaz@insa-toulouse.fr>
2024-10-14 10:36:28 +0200

    clock set

--- Debug/Core/Src/usart.d
+++ Debug/Core/Src/usart.d
@@ -10,27 +10,15 @@ Core/Src/usart.o: ../Core/Src/usart.c ../Core/Inc/usart.h \
  ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_bus.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_gpio.h
-
 ../Core/Inc/usart.h:
-
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_usart.h:
-
 ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
-
 ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
-
 ../Drivers/CMSIS/Include/core_cm4.h:
-
 ../Drivers/CMSIS/Include/cmsis_version.h:
-
 ../Drivers/CMSIS/Include/cmsis_compiler.h:
-
 ../Drivers/CMSIS/Include/cmsis_gcc.h:
-
 ../Drivers/CMSIS/Include/mpu_armv7.h:
-
 ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
-
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_bus.h:
-
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_gpio.h:
