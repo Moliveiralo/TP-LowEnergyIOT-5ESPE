27e8220f458913d04a370abb5dfb4712f8b11aee
MaximeO <maxime.oliveira40@gmail.com>
2024-12-04 08:30:51 +0100

    CODE POUR TESTER LE BLE

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -104,41 +104,42 @@ int main(void)
 	// 	}
 	// }
 
-//	//// ----------------------------------------------------------------------------
-//	//// 									PTX
-//	//// 					ENVOI DE MESSAGES VIA LE TRANCEIVER RF
-//	//// ----------------------------------------------------------------------------
-//	//configuration du transceiver en mode PTX
-//	Init_Transceiver();
-//	Config_RF_channel(channel_nb,nRF24_DR_250kbps,nRF24_TXPWR_18dBm);
-//	Config_CRC(CRC_Field_On, CRC_Field_1byte);
-//	//Adresse sur 5 bits. Transmission sur le data pipe adr_data_pipe_used.
-//	Config_PTX_adress(5,Default_pipe_address,adr_data_pipe_used,nRF24_AA_ON);
-//	Config_ESB_Protocol(nRF24_ARD_1000us,10);
-//	//on sort du mode power down
-//	nRF24_SetPowerMode(nRF24_PWR_UP);
-//	Delay_ms(2); //Attente 2 ms (1.5 ms pour la sortie du mode power down).
-//
-//	//Entrée en mode TX
-//	nRF24_SetOperationalMode(nRF24_MODE_TX);
-//	StopListen();
-//
-//	//configuration interruption Systick (attention, il n'y a quue 23 bits dans le registre load ...
-//	//mySystick( SystemCoreClock * 2 );	// 0.5 Hz --> 2 s
-//	//on va partir sur une période de 100 ms
-//	mySystick( SystemCoreClock /10 ); //10 Hz --> 0.1 s
-//
-//	int expNumber = 0;
-//	int packetNumber = 0;
-//
-//	// 1 char = 1 octet, donc chaque message a une taille max de 32 char
-//	char messageToSend[33];  // 32 char + caractère de fin de chaîne
-//
-//	// Création du message à envoyer			    |   NOMS DU BINOME   | EXPN | NBPAQUET |
-//	snprintf(messageToSend, sizeof(messageToSend), "O-LOPES_TETAZ_CHALHOUB_EXP%d_%d   ", expNumber, packetNumber);
-//
-//	// Appel de la fonction Transmit_Message (exemple d'appel)
-//	Transmit_Message((uint8_t *)messageToSend, 32);
+	//// ----------------------------------------------------------------------------
+	//// 									PTX
+	//// 					ENVOI DE MESSAGES VIA LE TRANCEIVER RF
+	//// ----------------------------------------------------------------------------
+
+	//configuration du transceiver en mode PTX
+	Init_Transceiver();
+	Config_RF_channel(channel_nb,nRF24_DR_250kbps,nRF24_TXPWR_18dBm);
+	Config_CRC(CRC_Field_On, CRC_Field_1byte);
+	//Adresse sur 5 bits. Transmission sur le data pipe adr_data_pipe_used.
+	Config_PTX_adress(5,Default_pipe_address,adr_data_pipe_used,nRF24_AA_ON);
+	Config_ESB_Protocol(nRF24_ARD_1000us,10);
+	//on sort du mode power down
+	nRF24_SetPowerMode(nRF24_PWR_UP);
+	Delay_ms(2); //Attente 2 ms (1.5 ms pour la sortie du mode power down).
+
+	//Entrée en mode TX
+	nRF24_SetOperationalMode(nRF24_MODE_TX);
+	StopListen();
+
+	//configuration interruption Systick (attention, il n'y a quue 23 bits dans le registre load ...
+	//mySystick( SystemCoreClock * 2 );	// 0.5 Hz --> 2 s
+	//on va partir sur une période de 100 ms
+	mySystick( SystemCoreClock /10 ); //10 Hz --> 0.1 s
+
+	int expNumber = 0;
+	int packetNumber = 0;
+
+	// 1 char = 1 octet, donc chaque message a une taille max de 32 char
+	char messageToSend[33];  // 32 char + caractère de fin de chaîne
+
+	// Création du message à envoyer			    |   NOMS DU BINOME   | EXPN | NBPAQUET |
+	snprintf(messageToSend, sizeof(messageToSend), "O-LOPES_TETAZ_CHALHOUB_EXP%d_%d   ", expNumber, packetNumber);
+
+	// Appel de la fonction Transmit_Message (exemple d'appel)
+	Transmit_Message((uint8_t *)messageToSend, 32);
 
 
 
@@ -241,6 +242,19 @@ void SysTick_Handler()
 	else 	old_blue = 0;
 	RTC->BKP1R = old_blue; //on passe l ancient etat au deuxieme registre
 	LED_GREEN(1);
+
+
+	int expNumber = 0;
+	int packetNumber = 0;
+
+	// 1 char = 1 octet, donc chaque message a une taille max de 32 char
+	char messageToSend[33];  // 32 char + caractère de fin de chaîne
+
+	// Création du message à envoyer			    |   NOMS DU BINOME   | EXPN | NBPAQUET |
+	snprintf(messageToSend, sizeof(messageToSend), "O-LOPES_TETAZ_CHALHOUB_EXP%d_%d   ", expNumber, packetNumber);
+
+	// Appel de la fonction Transmit_Message (exemple d'appel)
+	Transmit_Message((uint8_t *)messageToSend, 32);
 }
 
 
